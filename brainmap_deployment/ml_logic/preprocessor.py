646871923e6fd8ff4125a0d16669f4574ef0e99c
Gokhan-Dede <108541229+Gokhan-Dede@users.noreply.github.com>
2024-12-19 01:59:14 +0100

    Delete brainmap_deployment directory

--- brainmap_deployment/ml_logic/preprocessor.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-def normalize_mri(image):
-    """Normalize an MRI image tensor."""
-    mean, std = tf.reduce_mean(image), tf.math.reduce_std(image)
-    return (image - mean) / (std + tf.keras.backend.epsilon())
